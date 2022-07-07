9305e402f4f8a82736ac6c38f712bdedc55aa401
amebaleon <108870453+amebaleon@users.noreply.github.com>
2022-07-07 21:00:27 +0900

    tests

---
+++ test2.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	int b=1;
+	for(int a=10;a>0;a--)
+	{
+		b=b*a;
+	}
+	printf("%d",b);
+}
---
+++ test3.cpp
@@ -0,0 +1,16 @@
+#include <Stdio.h>
+int recu(int a)
+{
+	if(a<=1)
+	{
+		return (a-2);
+	}
+}
+int main()
+{	
+	
+	int a[100] = {0,};
+	scanf("%d",&a);
+	recu(a);
+	printf("%d",);
+}
